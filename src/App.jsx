9c3b990564a755bb8814a58580ab535f211ed232
Tommy Mönnich <tommy.moennich@check24.de>
2021-06-19 18:40:05 +0200

    Install material ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import React, { useState } from "react";
 import MainScreen from "./Components/MainScreen";
 import Menu from "./Components/Menu";
+import { Container, CssBaseline } from '@material-ui/core';
 
-function App() {
+export default function App() {
   const [showMenu, setShowMenu] = useState(false);
   return (
-    <div className="App">
-      <MainScreen setShowMenu={setShowMenu} />
-      <Menu setShowMenu={setShowMenu} showMenu={showMenu} />
-    </div>
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className="App">
+        <MainScreen setShowMenu={setShowMenu} />
+        <Menu setShowMenu={setShowMenu} showMenu={showMenu} />
+      </div>
+    </Container>
   );
 }
-
-export default App;

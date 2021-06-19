2e6bf5f05e5c706a7c4c06fbe21a8311991c2517
Tommy Mönnich <tommy.moennich@check24.de>
2021-06-19 18:48:28 +0200

    Apply global code styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
-import React, { useState } from "react";
-import MainScreen from "./Components/MainScreen";
-import Menu from "./Components/Menu";
-import { Container, CssBaseline } from '@material-ui/core';
+import React, { useState } from 'react';
+import MainScreen from './Components/MainScreen';
+import Menu from './Components/Menu';
 
-export default function App() {
+const App = () => {
   const [showMenu, setShowMenu] = useState(false);
   return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <div className="App">
-        <MainScreen setShowMenu={setShowMenu} />
-        <Menu setShowMenu={setShowMenu} showMenu={showMenu} />
-      </div>
-    </Container>
+    <div className='App'>
+      <MainScreen setShowMenu={setShowMenu} />
+      <Menu setShowMenu={setShowMenu} showMenu={showMenu} />
+    </div>
   );
 }
+
+export default App;

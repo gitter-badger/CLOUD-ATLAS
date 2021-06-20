e8ddf5a28f4e3811508d43b03c18064277b4edce
Tommy Mönnich <tommy.moennich@check24.de>
2021-06-20 09:54:46 +0200

    Split out menu icons into components

--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -1,19 +1,10 @@
 import React from "react";
+import MenuIconClose from './menu/MenuIconClose';
 
 const Menu = ({ showMenu, setShowMenu }) => {
   return (
     <div className={`menu full ${showMenu ? "show" : "hide"}`}>
-      <div className="menuIcon close" onClick={() => setShowMenu(false)}>
-        <svg
-          width="22"
-          height="22"
-          viewBox="0 0 22 22"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path d="M1 1L21 21M21 1L1 21" stroke="white" />
-        </svg>
-      </div>
+      <MenuIconClose setShowMenu={setShowMenu} />
     </div>
   );
 };
